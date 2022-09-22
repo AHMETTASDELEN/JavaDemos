b48ff53751cdc609ca949513fc4e1fda586f8321
AHMET TAŞDELEN <76402765+AHMETTASDELEN@users.noreply.github.com>
2022-09-23 00:01:36 +0300

    first commit
    first day homework

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi =12;
+        sayi = 1311111111;
+        byte sayi1 =12;
+        sayi1 = 127;
+        double sayi2 =12.5;
+        char karakter = 'A';
+        boolean dogrumMu=false;
+
+	}
+
+}
